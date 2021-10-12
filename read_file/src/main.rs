70047282b72e77177662741c540c3b5e26e96852
user <zanir.habib@gmail.com>
2021-10-11 20:13:36 -0500

    Finished assignment. Parsed url and did not print empty lines. Deleted comments.

--- read_file/src/main.rs
+++ read_file/src/main.rs
@@ -2,49 +2,39 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
+use url::{Url};
+
+fn parse_url(line:String) -> Option<Url>{
+    let test = Url::parse(&line);
+    match test{
+        Ok(url) => { println!("{}", url); return Some(url)}
+        Err(e) => return None,
+    }
+}
 
 fn main() {
 
 
     let f = File::open("./data/content.txt").expect("this didn't work");
 
-    // let f = match f {
-    //     Ok(file) => file,
-    //     Err(e) => panic!("Error:{}", e),
-    //     //Err(e) => println!("Error:{}", e),
-    //     // substitute this placeholder for the two possible patterns from the result type
-    //     // PATTERN => EXPRESSION,
-    //     // PATTERN => EXPRESSION,
-    // };
+
 
     let reader = BufReader::new(f);
     let mut count = 0;
 
     for line in reader.lines(){
         if let Ok(tested_line) = line{
-            println!("{}",tested_line);
+            if !(tested_line.is_empty()){
+                  parse_url(tested_line);  
+                
+            }
+
             count = count + 1;
         }
         
     }
     println!();
     println!("The file contains {} lines", count);
-
-    // let mut line = String::new();
-    // let len = reader.read_line(&mut line);
-    // println!("First line is {} bytes long", len);
-    //Ok(())
-
-
-   
-
-//     let mut reader = BufReader::new(f);
-//     //let mut file_content = String::new();
-
-
-
-
-//     //f.read_to_string(&mut file_content).expect("could not read file");
-//     //println!("File is {:?}",f)
+    
 
 }
